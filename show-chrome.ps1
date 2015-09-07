a5ba7ae195d60dc52b8a4b413948a4a3ffa003e8
Darrell Lowrance <darrell_lowrance@volusion.com>
2015-09-07 11:06:44 -0500

    Commit files through poshgit

---
+++ show-chrome.ps1
@@ -0,0 +1,9 @@
+﻿<#
+ # show processes
+#>
+
+Write-host "Show Chrome processes with CPU above 500"
+
+ps chrome | Where-Object {$_.CPU -gt "500"}
+
+
--- show-running-services.ps1
+++ show-running-services.ps1
@@ -1,8 +1,9 @@
 ﻿<#
  # show-running-services
  Gets services and shows only running services
-
+ 9.7.15 Added stuff...
+ .
 #>
-
+write-host "Show running services"
 Get-Service | where {$_.Status -eq "running"}
 
