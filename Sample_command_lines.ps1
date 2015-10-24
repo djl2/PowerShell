18f05cc2ba57bb4236f9f47cd5cc688968aa3b83
Darrell Lowrance <darrell_lowrance@volusion.com>
2015-10-24 15:59:10 -0500

    add new file

---
+++ Sample_command_lines.ps1
@@ -0,0 +1,21 @@
+﻿<#
+ # Sample command lines
+
+- Find the Windows Installer process
+
+ C:\Users\shralper\Documents\GitHub\PowerShell [master]> ps | ?{$_.Description -like "*install*"} | select -Property Description,name,id
+
+Description        Name      Id
+-----------        ----      --
+Windows® installer msiexec 4648
+Windows® installer msiexec 6128
+
+
+- Find the Windows Installer processes running and kill it.
+
+\PowerShell [master]> ps | ?{$_.Description -like "*install*"} | select -Property Description,name,id | stop-process -force
+
+
+
+
+#>
