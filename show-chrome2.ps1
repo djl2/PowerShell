e0e01fa263837a363d484109e9091c4fbc80cb76
Darrell Lowrance <darrell_lowrance@volusion.com>
2015-11-15 22:02:31 -0600

    checkin file

---
+++ show-chrome2.ps1
@@ -0,0 +1,23 @@
+﻿<#
+ # show processes
+#>
+
+
+Write-host "Show process with CPU utils above 500"
+
+
+function Get-ProcessAbove500 {
+param(
+[Parameter(Mandatory, HelpMessage=’Enter the process name please’)]
+$name
+)
+if (ps $name | Where-Object {$_.CPU -gt "500"})
+    {
+    "$name is above 500"
+    } else {
+        "$name is under 500"
+    }
+}
+
+
+
