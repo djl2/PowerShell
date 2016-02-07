e9b97466c8c876360ff0503ec66231c7dbf02018
Darrell Lowrance <darrell_lowrance@volusion.com>
2016-02-07 15:20:51 -0600

    update to latest

--- delete-files-w-params.ps1
+++ delete-files-w-params.ps1
@@ -1,21 +1,21 @@
 ﻿<#
 delete-files-w-params
-1.17.2016
+Latest rev 2.4.2016
 
 Deletes files recursively from a root folder
 
 Usage
   
-Run with correct parameters. By default, $TestOnly is set to $True to prevent accidental file deletion.
+By default, the parameter $DELETE is not defined (boolean value is effectively $false or 0) to prevent accidental file deletion.
 
-  .\delete-files-w-params.ps1 -RootFolder c:\temp -fileToDelete **-dev*.nupkg -DaysOld 90
+  delete-files-w-params.ps1 -RootFolder c:\temp -fileToDelete **-dev*.nupkg -DaysOld 30
 
-To delete files, run file with -TestOnly $false.  RUN WITH CAUTION!
+To delete files, run file with -DELETE $true.  RUN WITH CAUTION!
 
-  .\delete-files-w-params.ps1 -RootFolder c:\temp -fileToDelete **-dev*.nupkg -DaysOld 90 -TestOnly $false 
+  delete-files-w-params.ps1 -RootFolder c:\temp -fileToDelete **-dev*.nupkg -DaysOld 30 - DELETE $true 
 
 #>
- param (    # [string] $RootFolder= '\\aus02gpsvm01.corp.volusion.com\vol_nuget_tfs\Mozu\Nuget\nupkg\octodev_archive'    [string]$RootFolder = $(throw "Enter root folder. Parameter is -RootFolder"),    [string]$fileToDelete = $(throw "Enter filename to delete"),    [string]$DaysOld = $(throw "Enter number of days old"),    [boolean]$TestOnly = $true     )
+ param (    # Sample UNC path [string] $RootFolder= '\\aus02gpsvm01.corp.volusion.com\vol_nuget_tfs\Volusion\Nuget\nupkg\octodev_archive',    [string]$RootFolder = $(throw "Enter root folder. Parameter is -RootFolder"),    [string]$fileToDelete = $(throw "Enter filename to delete"),    [string]$DaysOld = $(throw "Enter number of days old"),    [boolean]$DELETE     )
 
 # Map an N drive to the root of the Nuget folder you want to delete files from
 New-PSDrive -Root $RootFolder -Name N -PSProvider filesystem
@@ -26,15 +26,16 @@ set-location N:
 # Command section
 Write-Host ''
 
-if ($TestOnly) # DEFAULT Runs with -whatif
-{
-    write-host "TEST ONLY! Delete any $fileToDelete files in root folder older than $DaysOld days..."
-    get-childitem -recurse | Where-Object {$_.lastwritetime -le (get-date).adddays(-$DaysOld) -and $_.Name -like $fileToDelete} | remove-item -Verbose -WhatIf
-}
-else
+# To delete files, run with parameter -DELETE $true
+if ($DELETE) 
 {
     write-host "Delete any $fileToDelete files in root folder older than $DaysOld days..."
-    get-childitem -recurse | Where-Object {$_.lastwritetime -le (get-date).adddays(-$DaysOld) -and $_.Name -like $fileToDelete} | remove-item -Verbose   
+    get-childitem -recurse | Where-Object {$_.lastwritetime -le (get-date).adddays($DaysOld) -and $_.Name -like $fileToDelete} | remove-item -Verbose
+}
+else # DEFAULT. No deletion. Runs with -whatif
+{
+    write-host "TEST ONLY! Delete any $fileToDelete files in root folder older than $DaysOld days..."
+    get-childitem -recurse | Where-Object {$_.lastwritetime -le (get-date).adddays($DaysOld) -and $_.Name -like $fileToDelete} | remove-item -Verbose -WhatIf
 }
 
 # Remove PSdrive
