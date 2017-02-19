9d281a4264807b92c36c38a11157cbdcb93ddb0a
Darrell Lowrance <dlowrance@ctintegrations.com>
2017-02-19 00:07:25 -0600

    Created for updating config files

---
+++ CTIntegrations/replace-strings.ps1
@@ -0,0 +1,43 @@
+﻿<#
+replace-strings.ps1
+Recursively finds and replaces values in all files in current folder 
+
+File names to search for
+
+*.exe.config
+web.config
+
+
+Strings to search for and replace
+
+cts30-dev
+sql2016
+192.168.0.95
+localhost <-- Use carefully! Some files have 'localhost' in the comments section
+
+
+/#>
+
+# Note: Replaced with param section
+# Variables Section ####################
+# $filespec = '*.exe.config'
+# $pattern = 'CTS30-DEV'
+
+# Override defaults as needed
+ param (
+  #  [string]$filespec = '*.exe.config',
+    [string]$filespec = '*.txt',
+    [string]$pattern = 'CTS30-DEV',
+    [string]$newPattern = 'CTS30-QA'
+    
+ )
+
+# Command Section ###################
+
+$collection = Get-ChildItem -Recurse $filespec | Where-Object { Select-String $pattern $_ -Quiet }
+
+foreach ($item in $collection)
+{
+    write-host "Replace $pattern in $item"
+   (Get-Content $item).Replace("$pattern", "$newPattern") | Set-Content $item
+}
