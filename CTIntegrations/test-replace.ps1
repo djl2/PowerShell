c5bd1d1dbb952d06af6118593b34b8aba145bd8b
Darrell Lowrance <dlowrance@ctintegrations.com>
2017-02-19 00:11:34 -0600

    add new

---
+++ CTIntegrations/test-replace.ps1
@@ -0,0 +1,17 @@
+﻿#
+
+ param (
+  #  [string]$filespec = '*.exe.config',
+    [string]$filespec = 'mytest.txt',
+    [string]$pattern = 'CTS30-DEV',
+    [string]$newPattern = 'CTS30-QA'
+    
+ )
+
+
+
+(get-content $filespec).Replace("$pattern","$newPattern") | Set-Content $filespec
+
+#
+
+# (get-content $myfile).Replace('prod','prodNEW') | Set-Content $myfile
