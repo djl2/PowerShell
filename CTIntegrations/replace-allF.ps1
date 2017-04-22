7179f2aa8623d64e572543b57c1471005938185c
Darrell Lowrance <dlowrance@ctintegrations.com>
2017-04-22 18:03:55 -0500

    New updates
    Made update

---
+++ CTIntegrations/replace-allF.ps1
@@ -0,0 +1,59 @@
+﻿<#
+replace-allF.ps1
+Custom function
+Recursively finds and replaces strings in all config files for new environments 
+
+File names to search for
+
+*.exe.config
+web.config
+
+
+Strings to search for and replace
+
+cts30-dev <-- Original host name
+sql2016  <-- Original SQL server
+192.168.0.95 <-- Original VM IP
+localhost <-- Use carefully! Some files have 'localhost' in the comments section
+
+
+/#>
+
+# Parameters section #####################
+# Override defaults as needed. By default, $newHostname will be automatically as the local hostname.
+ param (
+  #  [string]$filespec = '*.exe.config',
+    [string]$filespec = '**.config',
+    [string]$oldHostname = $( read-host "Enter old hostname."),
+    [string]$newHostname = "$env:COMPUTERNAME",
+    [string]$oldIP = $(Read-Host "Enter old IP address."),
+    [string]$newIP = $( Read-Host "Enter new IP address."),
+    [string]$oldSQL = $(Read-Host "Enter old SQL hostname"),
+    [string]$newSQL = $( Read-Host "Enter new SQL hostname")     
+ )
+
+
+# Function section #######################
+
+# Replace string values in all config files
+function update-configs ($param1, $param2)
+{
+$collection = Get-ChildItem -Recurse $filespec | Where-Object { Select-String $param1 $_ -Quiet }
+
+foreach ($item in $collection)
+  {
+    write-host "Replace $param1 in $item"
+   (Get-Content $item).Replace("$param1", "$param2") | Set-Content $item
+   write-host "Confirm $param2 was added to files"
+
+   Get-Content $item | Select-String -Pattern "$param2" -Quiet
+  }    
+}
+
+# Command Section ###################
+
+update-configs $oldHostname $newHostname
+
+update-configs $oldIP $newIP
+
+update-configs $oldSQL $newSQL
