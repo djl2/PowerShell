e31c0f5769e69e9ea79adaa01205d4df48429793
Darrell Lowrance <dlowrance@ctintegrations.com>
2017-04-23 14:34:48 -0500

    Add more strings for updating
    For local Avaya stack values in CTDeviceManager.exe.config.  Old
    password hidden

--- CTIntegrations/replace-allF.ps1
+++ CTIntegrations/replace-allF.ps1
@@ -1,20 +1,9 @@
 ﻿<#
 replace-allF.ps1
-Custom function
+Includes custom function update-configs
 Recursively finds and replaces strings in all config files for new environments 
 
-File names to search for
-
-*.exe.config
-web.config
-
-
-Strings to search for and replace
-
-cts30-dev <-- Original host name
-sql2016  <-- Original SQL server
-192.168.0.95 <-- Original VM IP
-localhost <-- Use carefully! Some files have 'localhost' in the comments section
+Hard reset to update all **.config files found. NEED TO CONFIRM!
 
 
 /#>
@@ -22,14 +11,40 @@ localhost <-- Use carefully! Some files have 'localhost' in the comments section
 # Parameters section #####################
 # Override defaults as needed. By default, $newHostname will be automatically as the local hostname.
  param (
-  #  [string]$filespec = '*.exe.config',
+    # Updates for CT Suite server values
+    # [string]$filespec = '*.exe.config',
     [string]$filespec = '**.config',
+    
+    # Updates for local host's name
     [string]$oldHostname = 'CTS30-QA',
     [string]$newHostname = "$env:COMPUTERNAME",
+    
     [string]$oldIP = '192.168.0.97',
-    [string]$newIP = $(Read-Host "Enter new IP address."),
+    [string]$newIP = $(Read-Host "Enter new CT Admin IP address"),
+    
     [string]$oldSQL = 'SQL2016QA',
-    [string]$newSQL = $(Read-Host "Enter new SQL hostname")     
+    [string]$newSQL = $(Read-Host "Enter new SQL hostname"),
+
+
+    # Updates for local Avaya stack values. For CTDeviceManager.exe.config
+    [string]$oldAESIPAddress = '192.168.0.111',
+    [string]$newAESIPAddress = $(Read-Host "Enter new AES IP Address"),
+    
+    [string]$oldAESUserName = 'ctsuitedev',
+    [string]$newAESUserName = $(Read-Host "Enter new AES User Name"),
+
+    [string]$oldAESPassword = 'CTPa33w0rd',
+    [string]$newAESPassword = $(Read-Host "Enter new AES Password"),
+    
+    [string]$oldAESProtocol = '7.0',
+    [string]$newAESProtocol = $(Read-Host "Enter new AES Protocol"),
+
+    [string]$oldAESCMName = 'auracm7',
+    [string]$newAESCMName = $(Read-Host "Enter new AES CM Name"),
+    
+    [string]$oldAESCMIPAddress = '192.168.0.110',
+    [string]$newAESCMIPAddress = $(Read-Host "Enter AES CM IP address")
+
  )
 
 
@@ -52,7 +67,15 @@ foreach ($item in $collection)
 
 # Command Section ###################
 
-# Update config files
+# Update CT Suite server files
 update-configs $oldHostname $newHostname
 update-configs $oldIP $newIP
 update-configs $oldSQL $newSQL
+
+# Updates for local Avaya stack values. For CTDeviceManager.exe.config
+update-configs $oldAESIPAddress $newAESIPAddress
+update-configs $oldAESUserName $newAESUserName
+update-configs $oldAESPassword $newAESPassword
+update-configs $oldAESProtocol $newAESProtocol
+update-configs $oldAESCMName $newAESCMName
+update-configs $oldAESCMIPAddress $newAESCMIPAddress
